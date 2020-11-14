cc7760b262a4cbffb3835dc8a3a93df2e2300d65
Luis Suarez <luissuac@gmail.com>
2020-11-14 11:37:02 -0500

    Initial Commit

---
+++ Pokedex-Luis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex-Luis
+//
+//  Created by LUIS SUAREZ on 14/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
